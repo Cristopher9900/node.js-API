fe443c8a1f86be5f8573fca59973420964da1bda
Cristopher9900 <cris.pichelli123@gmail.com>
2025-10-19 13:51:57 -0300

    :consreuction: node.js-API

---
+++ sever.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import {PrismaClient}from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express ()
+app.use(express.json())
+
+const users =[]
+
+app.post('/usuarios', (req, res)=>{
+    
+   prisma.user.create(
+    {
+        
+    }
+   )
+
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', (req, res) =>
+{ 
+
+    res.status(200).json(users)
+
+}
+)
+app. listen(3000)
+          
